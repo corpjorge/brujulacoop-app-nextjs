6607a1932c3e685a7b63aaa65cac73e261d09888
Jorge Eduardo <corpjorge@hotmail.com>
2022-03-18 12:11:18 -0500

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootlight/dist/css/bootlight.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import type {NextPage} from 'next'
+
+
+
+const dashboard: NextPage = () => {
+    return (
+        <div className="text-center mt-5">
+            dashboard
+        </div>
+    )
+}
+
+export default dashboard
