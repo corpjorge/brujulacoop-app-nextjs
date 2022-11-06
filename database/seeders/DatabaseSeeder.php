207cf407cec7c4d5aa2fe07f5e3a1b1a44f2de1b
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-11-05 20:33:38 -0500

    Agregamos seeder del tipo de pregunta para la encuesta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             QuestionSeeder::class,
             UserSeeder::class,
-            GiftSeeder::class
+            GiftSeeder::class,
+            SurveyQuestionTypeSeeder::class,
         ]);
     }
 }
