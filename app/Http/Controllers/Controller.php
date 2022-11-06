a989e82afe8b599015456c78019f9cc7dfd705c7
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-11-06 17:54:38 -0500

    Agregamos elementos de estado

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public $statuses = [
+        ['id' => 0, 'name' => 'Oculto'],
+        ['id' => 1, 'name' => 'Visible']
+    ];
 }
